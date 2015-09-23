82d9b46014272623e2e0fe5b48b7fc40012dfdc7
Jakob Borg <jakob@nym.se>
2015-09-23 22:25:16 +0200

    Transifex badges

--- plugin.rb
+++ plugin.rb
@@ -1,87 +1,55 @@
-# name: GitHub badges
-# about: Assign users badges based on GitHub contributions
+# name: Transifex translators
+# about: Assign users badges based on translation contributions
 # version: 0.1
 # authors: Sam Saffron
 
-module ::GithubBadges
+  require 'net/http'
+require 'json'
+
+module ::TransifexTranslators
   def self.badge_grant!
 
-    return unless SiteSetting.github_badges_repo.present?
+    return unless SiteSetting.transifex_translators_project.present?
 
-    # ensure badges exist
-    unless bronze = Badge.find_by(name: 'Hacker')
-      bronze = Badge.create!(name: 'Hacker',
-                             description: 'Contributed a commit',
+    def translators
+      url = URI.parse('http://www.transifex.com/api/2/project/' + SiteSetting.transifex_translators_project + '/languages/')
+      req = Net::HTTP::Get.new(url.to_s)
+      req.basic_auth SiteSetting.transifex_translators_user, SiteSetting.transifex_translators_pass
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+      }
+      data = JSON.parse(res.body)
+
+      users = []
+      data.each do |lang|
+        users << lang['coordinators'] + lang['reviewers'] + lang['translators']
+      end
+      users.uniq
+    end
+
+    unless bronze = Badge.find_by(name: 'Translator')
+      bronze = Badge.create!(name: 'Translator',
+                             description: 'Contributes translations',
                              badge_type_id: 3)
     end
 
-    unless silver = Badge.find_by(name: 'Great Hacker')
-      silver = Badge.create!(name: 'Great Hacker',
-                             description: 'Contributed 25 commits',
-                             badge_type_id: 2)
-    end
-
-    unless gold = Badge.find_by(name: 'Amazing Hacker')
-      gold = Badge.create!(name: 'Amazing Hacker',
-                             description: 'Contributed 250 commits',
-                             badge_type_id: 1)
-    end
-
-    emails = []
-
-    path = '/tmp/github_badges'
-
-    if !Dir.exists?(path)
-      Rails.logger.info `cd /tmp && git clone #{SiteSetting.github_badges_repo} github_badges`
-    else
-      Rails.logger.info `cd #{path} && git pull`
-    end
-
-    `cd #{path} && git log --pretty=format:%ae`.each_line do |m|
-      emails << m.strip
-    end
-
-    email_commits = emails.group_by{|e| e}.map{|k, l|[k, l.count]}
-
-    Rails.logger.info "#{email_commits.length} commits found!"
-
-    email_commits.each do |email, commits|
-      user = User.find_by(email: email)
+    translators.each do |name|
+      user = User.find_by(username: name)
 
       if user
-        if commits < 25
-          BadgeGranter.grant(bronze, user)
-          if user.title.blank?
-            user.title = bronze.name
-            user.save
-          end
-        elsif commits < 250
-          BadgeGranter.grant(silver, user)
-          if user.title.blank? or user.title == bronze.name
-            user.title = silver.name
-            user.save
-          end
-        else
-          BadgeGranter.grant(gold, user)
-          if user.title.blank? or user.title == bronze.name or user.title == silver.name
-            user.title = gold.name
-            user.save
-          end
-        end
+        BadgeGranter.grant(bronze, user)
       end
-
     end
-
   end
 end
 
 after_initialize do
-  module ::GithubBadges
+  module ::TransifexTranslators
     class UpdateJob < ::Jobs::Scheduled
       every 1.day
 
       def execute(args)
-        GithubBadges.badge_grant!
+        TransifexTranslators.badge_grant!
       end
     end
   end
